b9040f3cd830c4c09d835d4bf024fcfc979b5863
Vicky <vicky@Vickys-MBP.default>
2020-04-05 14:58:43 +0100

    starting the project

--- src/App.js
+++ src/App.js
@@ -1,86 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person';
-import Stick from './Stick/Stick';
-import Hook from './Hook/Hook';
+
 
 
 
 class App extends Component {
-
-  
-  state = {
-    persons: [
-      { name: "Viki", age: 28 },
-      { name: "Maria", age: 23 },
-      { name: "Jorge", age: 20 }
-    ],
-    showPersons: false,
-    otherState: "some other value"
-  }
-
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: "Nik", age: 57 },
-        { name: "Stephanie", age: 20 }
-      ]
-
-    })
-  }
-
-  deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons.slice();
-    persons.splice(personIndex, 1);
-    this.setState({ persons: persons});
-  }
-
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: "Max", age: 28 },
-        { name: event.target.value, age: 57 },
-        { name: "Jorge", age: 20 }
-      ]
-    })
-  }
-
-  togglePersonsHnadler = () => {
-    const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow });
-  }
-
   render() {
 
-    let persons = null;
-
-    if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person
-                key={person.id}
-                //click={this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                changed={this.nameChangedHandler} />
-           
-          })}
-        </div>
-      );
-    }
+  
+          
     return (
       <div className="App">
-        <h1>Welcome to my Udemy app</h1>
-        <button onClick={this.togglePersonsHnadler}>Toggle Persons</button>
-        {persons}
-        <Stick/>
-        <Hook/>
+      
       </div>
     );
   }
-}
+  
+  
+  }
+
+
+  
+
 
 
 export default App;
