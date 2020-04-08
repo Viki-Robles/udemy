64d01439db1c0e16f82584f910304831fd759bfc
Vicky <vicky@Vickys-MBP.default>
2020-04-08 21:15:57 +0100

    adding the reduce method

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -8,7 +8,20 @@ export default function Burger(props) {
         return [...Array(props.ingredients[idKey])].map((_, i)=> {
             return <BurgerIngredient key={idKey+i} type={idKey}/>;
         });
-    });
+    })
+    //This will allow to the user when is an empty Burger to receive a pop up message to start filling it. 
+    //This process below is creates the scenario of an empty array when all the ingredient are 0 - empty
+    //.reduce() is a build array function that allows us to transform an array into something else. 
+    //It receives 2 arguments past value and new value
+    // the empty value is the initial array
+    //then we loop to the elements with the return and add them to the initial value step by step
+    .reduce((arr, el) => {
+        return arr.concat(el);
+    }, []);
+
+    if (transformIngredients.length === 0) {
+        transformIngredients = <p>Please start adding ingredients!</p>
+    }
     return (
         <>
             <div className="Burger">
